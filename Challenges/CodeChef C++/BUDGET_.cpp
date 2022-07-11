948de05c2e7493ddc1ba822661d121534f684946
narie <monarijob@yahoo.com>
2022-07-11 21:45:50 +0300

    changes

--- Challenges/CodeChef C++/BUDGET_.cpp
+++ Challenges/CodeChef C++/BUDGET_.cpp
@@ -9,17 +9,17 @@ using namespace std;
 #define loop(x, n) for (int x = 0; x < n; ++x)
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	int a, b, c;
-	cin >> a;
-	while (a--) {
-		cin >> b >> c;
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int a, b, c;
+  cin >> a;
+  while (a--) {
+    cin >> b >> c;
 
-		if (b >= (c * 30))
-			cout << "YES\n";
-		else
-			cout << "NO\n";
-	}
+    if (b >= (c * 30))
+      cout << "YES\n";
+    else
+      cout << "NO\n";
+  }
 }
 
--- Challenges/CodeChef C++/COURSEREG.cpp
+++ Challenges/CodeChef C++/COURSEREG.cpp
@@ -2,29 +2,23 @@
 
 #include <algorithm>
 #include <cmath>
-#include <cstdio>
 #include <iostream>
 #include <string>
 #include <vector>
-#define loop(x, n) for (int x = 0; x < n; ++x)
-
 using namespace std;
+#define loop(x, n) for (int x = 0; x < n; ++x)
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	int a;
+	int a, b, c, d;
 	cin >> a;
 	while (a--) {
-		int b;
-		cin >> b;
-		int count = 0;
-		string s;
-		cin >> s;
-		loop(i, b) {
-			if (s[i] == '1' && s[i + 1] == '0') count++;
-		}
-		cout << count << endl;
+		cin >> b >> c >> d;
+		if ((b + d) > c)
+			cout << "NO\n";
+		else
+			cout << "YES\n";
 	}
 }
 
